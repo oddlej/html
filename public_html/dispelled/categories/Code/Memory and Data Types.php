283405460ed55d1f5bfd14b25bc9a26cd53abcd8
oddlej <oddlej@proton.me>
2024-11-02 12:59:45 -0600

    check all

--- public_html/dispelled/categories/Code/Memory and Data Types.php
+++ public_html/dispelled/categories/Code/Memory and Data Types.php
@@ -33,15 +33,13 @@
 <pre>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15</pre>
 <p>A is 10, B is 11, C is 12, D is 13, E is 14, F is 15.</p>
 
-<a href="https://learn.sparkfun.com/tutorials/binary/bits-nibbles-and-bytes">Here's</a> a link. Each column is a bit. Four Bits 0000 (decimal 0) are a Nibble, and eight Bits 00000000 (this example is also Decimal 0 but longer) are a Byte. 8 digits (8 Bits make a Byte) in Binary can be shown in Hexidecimal with two digits (still one Byte).</p>
+<p><a href="https://learn.sparkfun.com/tutorials/binary/bits-nibbles-and-bytes">Here's</a> a link. Each column is a bit. Four Bits 0000 (decimal 0) are a Nibble, and eight Bits 00000000 (this example is also Decimal 0 but longer) are a Byte. 8 digits (8 Bits make a Byte) in Binary can be shown in Hexidecimal with two digits (still one Byte).</p>
 
-<p> Ok. so let's convert Binary Bytes to Hex Bytes.</p>
+<p>Ok. so let's convert Binary Bytes to Hex Bytes.</p>
 
-<pre>
-00111110 in binary is
+<pre>00111110 in binary is
 3E in Hexidecimal and
-62 in Decimal
-</pre>
+62 in Decimal</pre>
 
 <p><a href="https://www.rapidtables.com/convert/number/binary-to-hex.html?x=00111110">Here</a> double check me.</p>
 
@@ -49,40 +47,32 @@
 
 <p>At this point we are halfway through data types so we might as well keep going. It is also important to recognize at this point how we show that we are using Binary or Hexidecimal values. For binary 00111110 we append 0B</p>
 
-<pre>
-0B00111110
-</pre>
+<pre>0B00111110</pre>
 
-<p>And for Hexidecimal we append an 0X
+<p>And for Hexidecimal we append an 0X</p>
 
-<pre>
-0X3E
-</pre>
+<pre>0X3E</pre>
 
 <p>For Decimal we can use 0D. We will continue with Hex for now so there is no need to append these. Just to know if you see 0B or 0X it is to show what the value is.</p>
 
-<p>As We have stated, Eight Bits make a Byte. Continuing on with Hexidecimal, a Byte is two columns (3E). From there we move up to a Halfword (003E). A Halfword is four Bytes, and a < href="https://en.wikipedia.org/wiki/Word_(computer_architecture)">Word</a> (0000003E) is eight. Before we go too far, lets scale our bits.</p>
+<p>As We have stated, Eight Bits make a Byte. Continuing on with Hexidecimal, a Byte is two columns (3E). From there we move up to a Halfword (003E). A Halfword is four Bytes, and a <a href="https://en.wikipedia.org/wiki/Word_(computer_architecture)">Word</a> (0000003E) is eight. Before we go too far, lets scale our bits.</p>
 
-<pre>
-3E					// Byte (8 Bits)
-003E				// Halfword (16 Bits)
-0000003E			// Word (32 Bits)
-000000000000003E	// Doubleword (64 bits)
-</pre>
+<pre>3E					// Byte (8 Bits)
+003E					// Halfword (16 Bits)
+0000003E				// Word (32 Bits)
+000000000000003E		// Doubleword (64 bits)</pre>
 
 <p>These all represent the Hexidecimal value 3E, which is 00111110 in binary and 62 in Decimal.</p>
 
 <p>Now lets look. The difference in the extra columns is the amount you can address with the extra space. In a Halfword example:</p>
 
-<pre>
-0xFFFF = 0D65535
-FFFF = 65335
-</pre>
+<pre>0xFFFF = 0D65535
+FFFF = 65335</pre>
 
 <p>16 bits in Binary. (0B1111111111111111).</p>
 <p>A Word (FFFFFFFF) in Hex equates to the number (4294967295) in Decimal. After that there is Doublewords and such, but the numbers are so large we are already using a converter anyways. So let's stop there. Next is Signed and Unsigned values and that is another chapter lol. Hope I was able to help somewhat.</p>
 
-
+<?php include("../../footer.php"); ?>
 
 
 
