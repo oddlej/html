28b40797d21c78d8503a17da6b36a0490d72eda0
Oddlej <oddlej@proton.me>
2025-02-03 22:18:13 -0600

    Add bg

--- public_html/dispelled/categories/Recipies/Beer Caesars.php
+++ public_html/dispelled/categories/Recipies/Beer Caesars.php
@@ -4,14 +4,35 @@
 
 <h1>Beer Caesars</h1>
 
-<p id="firstHeading" class="firstHeading" lang="en">So maybe my pictures don’t do it justice, but this is a damn fine drink. A slight variation on the ol’ Beer n Clam. Additions are pickles, pickle juice, pickled beans, <a href="https://en.wikipedia.org/wiki/Sriracha_sauce">sriracha sauce</a>, <a href="http://www.uvvodka.com/products/sriracha">sriracha vodka</a>, vodka, pepper, bacon rim spice, really whatever a regular caesar would get. This is my favorite.</p>
-<pre>1 Beer (Coors Light for me)
-3 Ice cubes
-Clamato to top
-4 Banana pepper rings
-Splash of Banana pepper juice 1/2 tsp Creamed Horseradish</pre>
-<p>Stir that all up with a fork and have a vodka shot first. Delicious.</p>
-<p><a href="/images/wpid-20150903_000739.jpg"><img class="alignnone size-full" title="20150903_000739.jpg" src="/images/wpid-20150903_000739.jpg" alt="image"></a></p>
+<p id="firstHeading" class="firstHeading" lang="en">So maybe my pictures don’t do it justice, but this is a damn fine drink. A slight variation on the ol’ Beer n Clam. It's perfect for a summer day or daily after work lol.</p>
+
+<h2>Ingredients</h2>
+<ul>
+    <li>1 bottle of Beer (Coors Banquet for me)</li>    
+    <li>1 cup Clamato juice</li>
+    <li>2 Ice cubes</li>
+    <li>1/2 Tsp Horseradish</li>
+    <li>Additions to taste or depending on the day or time of year</li>
+    <li>Today I will be adding a splash of Banana Pepper juice and 4 Banana Pepper rings.</li>
+    <li>1 shot of White Rum</li>
+</ul>
+
+<h2>Instructions</h2>
+<pre>
+<code class="language-plaintext">
+1. Add the Clamato to a large glass.
+2. Add the Ice cubes
+3. Pour in Beer to the top.
+4. Add your preferred additions into the glass.
+5. Stir that all up with a fork and have a Rum shot first. Delicious.
+6. Cheers!
+</code>
+</pre>
+
+Additions are banana pepper rings, banana pepper juice, pickles, pickle juice, pickled beans, Franks Red Hot, <a href="https://en.wikipedia.org/wiki/Sriracha_sauce">Sriracha sauce</a>, pepper, bacon rim spice, really whatever a regular caesar would get. Cheers!</p>
+
+
+<p><a href="/images/wpid-20150903_000739.jpg"><img title="20150903_000739.jpg" src="/images/wpid-20150903_000739.jpg" width="90%"></a></p>
 
 
 <?php include("../../footer.php"); ?>
