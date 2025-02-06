eb85fca745e8b0675f940a930656e158824d5455
Oddlej <oddlej@proton.me>
2025-02-06 08:22:58 -0600

    add unix

---
+++ public_html/dispelled/categories/Unix/index.php
@@ -0,0 +1,6 @@
+<?php include("../../headercat.php"); ?>
+<h1>Unix</h1>
+<p>And Linux, BSD, etc. Everyhting that doesn't fit in computers and code ;)</p>
+<?php include("../../cat.php"); ?>
+
+<?php include("../../footer.php"); ?>
