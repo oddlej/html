e7a9314e4d67f46ea815e6916aa2cbc3bb8fac8d
Oddlej <oddlej@proton.me>
2025-05-24 15:17:57 -0600

    really?

--- public_html/dispelled/j.php
+++ public_html/dispelled/j.php
@@ -3,19 +3,20 @@
 <h1>J&#8217;s stuff</h1>
 
 <p><strong>Weather</strong><br>
-
 <a href="http://www.theweathernetwork.com/weather/cask0214">Weather Network</a>, <a href="https://hotline.gov.sk.ca/map">SK Highway Hotline</a>, <a href="https://weather.gc.ca/city/pages/sk-51_metric_e.html">Weather Canada</a>, <a href="https://www.wunderground.com/weather/ca/mossbank/49.94,-105.96">Weather Underground</a></p>
 
 <p><strong>Coffee Time</strong><br>
+<a href="https://gab.com/">Gab</a>, <a href="http://reddit.com/">Reddit</a>, <a href="http://slashdot.org/">Slashdot</a>, <a href="http://arstechnica.com/">Ars Technica</a>, <a href="https://forum.netgate.com/">Netgate Forums</a>, <a href="https://thelinuxcode.com/">The Linux Code</a>, <a href="https://www.typing.com/">Typing</a>, <a href="https://www.chess.com/play/computer">Chess</a>, <a href="https://yousician.com/">Yousician</a>, <a href="https://www.bible.com/reading-plans">Bible.com</a>, <a href="https://www.facebook.com/norquaycovenantchurch/">Norquay Covenant Church</a></p>
 
-<a href="https://animeday.ca">Animeday.ca</a>, <a href="https://yousician.com/">Yousician</a>, <a href="https://gab.com/">Gab</a>, <a href="https://app.beartariatimes.com/">The Beartaria Times App</a>, <a href="http://reddit.com/">Reddit</a>, <a href="https://rumble.com/">Rumble</a>, <a href="https://www.bitchute.com/">Bitchute</a>, <a href="https://www.wnd.com/">WND</a>, <a href="https://infowars.com/">Infowars</a>, <a href="https://www.thinkspot.com/">Thinkspot</a>, <a href="https://firstlook.org/theintercept/">The Intercept</a>, <a href="https://minds.com/">Minds</a>, <a href="http://slashdot.org/">Slashdot</a>, <a href="http://arstechnica.com/">Ars Technica</a>, <a href="https://www.typing.com/">Typing</a>, <a href="https://www.chess.com/play/computer">Chess</a>, <a href="https://www.bible.com/reading-plans">Bible.com</a>, <a href="https://cornerstonegospelchurch.com/sermons">Cornerstone Sermons</a>
+<p style="margin-left:30px;"><strong>Other Media</strong><br>
+<a href="https://www.thinkspot.com/">Thinkspot</a>, <a href="https://app.beartariatimes.com/">The Beartaria Times App</a>, <a href="https://rumble.com/">Rumble</a>, <a href="https://www.bitchute.com/">Bitchute</a>, <a href="https://www.wnd.com/">WND</a>, <a href="https://infowars.com/">Infowars</a>
 </p>
 
 <p><strong>Music</strong><br>
 <a href="https://ableton.com/">Ableton</a> (<a href="https://forum.ableton.com/">Forum</a>, <a href="https://www.ableton.com/en/manual/live-concepts/">Manual</a>, <a href="https://www.reddit.com/r/ableton/">Reddit</a>, <a href="https://www.reddit.com/r/abletonlive/">Live Reddit</a>), <a href="https://www.ableton.com/en/push/">Push 2</a> (<a href="https://forum.ableton.com/viewforum.php?f=55">Forum</a>, <a href="https://www.ableton.com/en/manual/using-push-2/">Manual</a>, <a href="https://www.reddit.com/r/AbletonPush2/">Reddit</a>), <a href="https://focusrite.com/">Focusrite</a>, <a href="https://rolandcloud.com/">Roland Cloud</a>, <a href="https://www.xlnaudio.com/products/addictive_drums_2">Addictive Drums</a>, <a href="https://www.songsterr.com/">Songsterr</a>, <a href="https://scottsbasslessons.com/">Scott's Bass Lessons</a>, <a href="http://www.juststrings.com/">Juststrings</a>
 </p>
 
-<p style="margin-left:30px;"><strong>Effects</strong><br>
+<p style="margin-left:30px;"><strong>VST Effects</strong><br>
 <a href="https://www.positivegrid.com/">Positive Grid</a> (Bias <a href="https://www.positivegrid.com/bias-amp/">AMP</a>, <a href="https://www.positivegrid.com/bias-fx/">FX</a> and <a href="https://www.positivegrid.com/bias-pedal/">Pedal</a>), <a href="https://valhalladsp.com/">Valhalla DSP</a> 
 </p>
 
@@ -61,6 +62,14 @@
 <a href="https://proton.me/">Proton</a>, <a href="https://shopify.com/">Shopify</a>, <a href="https://namecheap.com/">Namecheap</a>, <a href="https://www.whatismyip.com">What is my Ip</a>, </a><a href="https://www.dnsleaktest.com/">DnsLeakTest</a>, <a href="https://decoder.link/sslchecker/">SSL Checker</a>, <a href="https://www.mail-tester.com/">Mail Tester </a><a href="https://www.mail-tester.com/spf-dkim-check">(SPF and DKIM Check)</a>, <a href="https://dkimvalidator.com/">DKIM Validator</a>, <a href="https://www.fontsquirrel.com/tools/webfont-generator">Font Squirrel</a>, <a href="https://favicon.io/favicon-converter/">Favicon.io</a>, <a href="http://tools.pingdom.com/fpt/">Pingdom</a>, <a href="https://opendns.com/">OpenDNS</a>, <a href="https://speedtest.net/">Speedtest.net</a>, <a href="https://fast.com/">Fast.com</a>, <a href="https://validator.w3.org/">W3C Markup Validator</a>
 </p>
 
+<p><strong>Shopping</strong><br>
+<a href="https://amazon.ca">Amazon</a>, <a href="https://Temu.com">Temu</a>, <a href="https://Digikey.com">Digikey</a>, <a href="https://Costco.ca">Costco</a>
+</p>
+
+<p><strong>Better Shopping - Skateboarding, Biking and Shades</strong><br>
+<a href="http://a51.ca">Area 51 Skateshop</a>, <a href="https://primaryskateboards.ca/">Primary Skateboards</a>, <a href="https://www.thebikeshop.com/">The Bike Shop Calgary</a>, <a href="https://www.a51.ca/skateshop/shop/category_search/?search_with_change_category_ability=1&categoryID=645&search_name=&search_price_from=&search_price_to=&param_16=15&search_in_subcategory=1&advanced_search_in_category=Find">Area 51&#8217;s Globe Shoes</a>, <a href="https://fostergrant.com/">Foster Grant</a>
+</p>
+
 <p><strong>Buy and Sell</strong><br>
 <a href="https://ebay.ca/">EBay</a>, <a href="https://www.kijiji.ca/b-musical-instrument/moose-jaw/c17l1700195">Kijiji Musical Instruments</a>, <a href="http://www.usedregina.com/classifieds/musical-instruments">Used Regina Musical Instruments</a>, <a href="https://www.kijiji.ca/b-classic-cars/saskatchewan/ford/k0c122l9009">Sask. Kijiji Classic Cars - Ford</a></p>
 
@@ -87,16 +96,14 @@
 <a href="https://www.ifit.com/">IFit Dashboard</a>, <a href="https://log.concept2.com/log">Concept2 Rowing Logbook</a>, <a href="http://stronglifts.com/5x5/">Stronglifts 5&#215;5</a>, <a href="https://www.myfitnesspal.com/">Myfitnesspal</a>, <a href="https://www.t-nation.com/">T-Nation</a>
 </p>
 
-<p><strong>Skateboarding, Biking and Shades</strong><br>
-<a href="http://a51.ca">Area 51 Skateshop</a>, <a href="https://primaryskateboards.ca/">Primary Skateboards</a>, <a href="https://www.thebikeshop.com/">The Bike Shop Calgary</a>, <a href="https://www.a51.ca/skateshop/shop/category_search/?search_with_change_category_ability=1&categoryID=645&search_name=&search_price_from=&search_price_to=&param_16=15&search_in_subcategory=1&advanced_search_in_category=Find">Area 51&#8217;s Globe Shoes</a>, <a href="https://fostergrant.com/">Foster Grant</a>
-</p>
-
 <p><strong>Games</strong><br>
 <a href="https://hsr.hoyoverse.com/en-us/">Honkai Star Rail</a>, <a href="https://www.reddit.com/r/HonkaiStarRail/">HSR Reddit</a>, <a href="https://genshin.hoyoverse.com/en/">Genshin Impact</a>,
 <a href="https://www.reddit.com/r/Genshin_Impact/">Genshin Reddit</a>, <a href="https://www.hoyolab.com/home">HoyoLab</a>
 </p>
 
 <!-- ================================ END OF VISIBLE ============================ 
+
+<a href="https://animeday.ca">Animeday.ca</a>, 
 
 <p><strong>More Conversions</strong></br>
 <a href="https://rapidtables.com/web/tools/base64-decode.html">Base64 decoder/encoder</a>, 
